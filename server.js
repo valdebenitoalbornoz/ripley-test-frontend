f93095067024684dd02ab4dd43275d33dd0b4679
Daniel Valdebenito <daniel.valdebenito@idiem.cl>
2021-07-12 22:51:01 -0400

    Prepare to deployment

---
+++ server.js
@@ -0,0 +1,13 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/app-test'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/app-test/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
